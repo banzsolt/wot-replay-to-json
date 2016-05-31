66a722342ef097539af9c9120b4ba583735887a8
Zsolt Ban <banzsolt94@gmail.com>
2016-05-31 23:31:11 +0100

    First replay file

--- lib/wot_replay_to_json.rb
+++ lib/wot_replay_to_json.rb
@@ -1,4 +1,5 @@
-require "wot_replay_to_json/version"
+require 'wot_replay_to_json/version'
+require 'wot_replay_to_json/replay_parser'
 
 module WotReplayToJson
   # Your code goes here...
---
+++ lib/wot_replay_to_json/replay_parser.rb
@@ -0,0 +1,10 @@
+module WotReplayToJson
+  class WotReplayToJson::ReplayParser
+
+    contents = File.read('C:/Users/Zsolti/Documents/wot_replay_to_json/lib/wot_replay_to_json/20160219_2336_ussr-R109_T54S_44_north_america.wotreplay')
+
+    puts contents
+
+  end
+end
+
